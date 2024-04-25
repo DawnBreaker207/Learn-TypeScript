26d9e74c8385d95aaf5ef5ffab74786cb55d44e4
DawnBreaker <tunganhngo207@gmail.com>
2024-04-25 13:05:33 +0700

    Index types

--- Advanced_Types/src/app.ts
+++ Advanced_Types/src/app.ts
@@ -123,3 +123,13 @@ const userInputElement = document.getElementById('user-input');
 if (userInputElement) {
   (userInputElement as HTMLInputElement).value = 'Hi there';
 }
+
+// Index Types
+interface ErrorContainer {
+  [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+  email: 'Not a valid email!',
+  username: 'Must start with a capital character!',
+};
